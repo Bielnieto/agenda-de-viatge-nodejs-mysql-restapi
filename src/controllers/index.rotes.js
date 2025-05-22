f1ee931b3d6924287b21a082c950ebb8814c8b2f
Biel <biel.nieto@mataro.epiaedu.cat>
2025-05-22 21:21:20 +0200

    update

--- src/controllers/index.rotes.js
+++ src/controllers/index.rotes.js
@@ -6,3 +6,12 @@ export const index = (req, res) => res.json({ message: "welcome to my api" });
 export const ping = (req, res) => {
   res.json({ result: "pong" });
 };
+
+export const clients = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM clients");
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving clients" });
+  }
+};
